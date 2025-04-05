01347e2bd47d6e9fb58701c64eedeaddce1b5a8c
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2025-04-05 13:14:19 -0300

    agregado verificacion de respuestas

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+import {
+  generarPreguntaNivel1,
+  generarPreguntaNivel2,
+  generarPreguntaNivel3,
+  generarPreguntaNivel4,
+  getRandom,
+} from "./PreguntasNiveles.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   const preguntaCargar = generarPreguntaNivel1();
-  const divPregunta = document.getElementById("pregunta");
-  const divRespuesta1 = document.getElementById("respuesta1");
-  const divRespuesta2 = document.getElementById("respuesta2");
-  const divRespuesta3 = document.getElementById("respuesta3");
-  const divRespuesta4 = document.getElementById("respuesta4");
 
-  const listaRespuestas = [
-    divRespuesta1,
-    divRespuesta2,
-    divRespuesta3,
-    divRespuesta4,
-  ];
+  mezclarRespuestas(preguntaCargar);
+});
 
-  // mezclar respuestas
+function mezclarRespuestas(preguntaCargar) {
+  const { divPregunta, listaRespuestas } = preguntasRespuestasDocument();
+
   const respuestasShuffled = preguntaCargar.respuestas
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
@@ -23,22 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
   listaRespuestas.forEach((div, index) => {
     div.textContent = respuestasShuffled[index];
   });
-});
+}
 
 document.addEventListener("click", CargarYVerificarRespuesta);
 
 function CargarYVerificarRespuesta(event) {
-  const divPregunta = document.getElementById("pregunta");
-  const divRespuesta1 = document.getElementById("respuesta1");
-  const divRespuesta2 = document.getElementById("respuesta2");
-  const divRespuesta3 = document.getElementById("respuesta3");
-  const divRespuesta4 = document.getElementById("respuesta4");
+  const { divPregunta, listaRespuestas } = preguntasRespuestasDocument();
 
   if (
-    event.target === divRespuesta1 ||
-    event.target === divRespuesta2 ||
-    event.target === divRespuesta3 ||
-    event.target === divRespuesta4
+    event.target === listaRespuestas[0] ||
+    event.target === listaRespuestas[1] ||
+    event.target === listaRespuestas[2] ||
+    event.target === listaRespuestas[3]
   ) {
     const respuestaSeleccionada = parseInt(event.target.textContent);
     const correcta =
@@ -52,93 +49,22 @@ function CargarYVerificarRespuesta(event) {
   }
 }
 
-function generarPreguntaNivel1() {
-  // let pregunta = "";
-  // const digitos = "0123456789";
-  //const operadores = "+-*/";
-  // const indiceOperador = Math.floor(Math.random() * 4);
-  // const operadorRandom = operadores[indiceOperador];
-  let primerNum = Math.floor(Math.random() * 9 + 1);
-  let segundoNum = Math.floor(Math.random() * 9 + 1);
-  const correcta = primerNum + segundoNum;
-  let minima = correcta - 10;
-  let maxima = correcta + 10;
-  const operador = "+";
-  let respuestas = [
-    correcta,
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
+function preguntasRespuestasDocument() {
+  const divPregunta = document.getElementById("pregunta");
+  const divRespuesta1 = document.getElementById("respuesta1");
+  const divRespuesta2 = document.getElementById("respuesta2");
+  const divRespuesta3 = document.getElementById("respuesta3");
+  const divRespuesta4 = document.getElementById("respuesta4");
+
+  const listaRespuestas = [
+    divRespuesta1,
+    divRespuesta2,
+    divRespuesta3,
+    divRespuesta4,
   ];
-  const pregunta = {
-    enunciado: `${primerNum} ${operador} ${segundoNum}`,
-    respuestas,
-    correcta: 0,
+
+  return {
+    divPregunta,
+    listaRespuestas,
   };
-
-  return pregunta;
-}
-
-function generarPreguntaNivel2() {
-  let primerNum = Math.floor(Math.random() * 50 + 1);
-  let segundoNum = Math.floor(Math.random() * 25 + 1);
-  const correcta = primerNum + segundoNum;
-  let minima = correcta - 15;
-  let maxima = correcta + 15;
-  const operador = "+";
-  let respuestas = [
-    correcta,
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-  ];
-  const pregunta = {
-    enunciado: `${primerNum} ${operador} ${segundoNum}`,
-    respuestas,
-    correcta: 0,
-  };
-}
-
-function generarPreguntaNivel3() {
-  let primerNum = getRandom(5, 500);
-  let segundoNum = getRandom(1, 100);
-  const correcta = primerNum + segundoNum;
-  let minima = correcta - 20;
-  let maxima = correcta + 20;
-  const operador = "+";
-  let respuestas = [
-    correcta,
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-  ];
-  const pregunta = {
-    enunciado: `${primerNum} ${operador} ${segundoNum}`,
-    respuestas,
-    correcta: 0,
-  };
-}
-
-function generarPreguntaNivel4() {
-  let primerNum = getRandom(500, 1200);
-  let segundoNum = getRandom(100, 500);
-  const correcta = primerNum + segundoNum;
-  let minima = correcta - 50;
-  let maxima = correcta + 50;
-  const operador = "+";
-  let respuestas = [
-    correcta,
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-    getRandom(minima, maxima),
-  ];
-  const pregunta = {
-    enunciado: `${primerNum} ${operador} ${segundoNum}`,
-    respuestas,
-    correcta: 0,
-  };
-}
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
 }
